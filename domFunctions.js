e89306419739b457867c973679a66f6d7da99407
Tom Kondat <tomkondat5@gmail.com>
2024-07-12 18:50:12 +0300

    Try to add modal

--- domFunctions.js
+++ domFunctions.js
@@ -14,9 +14,52 @@ const createSingleMovieCard = (filmObj) => {
   </div>
  </div>
     `;
-  console.log(filmObj);
-  content.append(filmCardEl);
+  // Create the button
+  const button = document.createElement("button");
+  button.textContent = "Open Modal";
+  button.className = "open-modal-button";
+
+  // Add event listener to the button
+  button.addEventListener("click", () => {
+    createModalForMovie(filmObj);
+  });
+
+  // Append the button to the film card
+  filmCardEl.appendChild(button);
+
   return filmCardEl;
 };
 
 export { createSingleMovieCard };
+
+const createModalForMovie = () => {
+  const modal = document.createElement("div");
+  modal.className = "modal";
+  modal.innerHTML = `
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+  return modal;
+};
+
+export { createModalForMovie };
